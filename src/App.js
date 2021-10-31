6da6b3dcb8e8ef94aef5f967c0724b096047eefa
RobsonMaestro <robson_maestro@alumni.usp.br>
2021-10-31 12:27:12 -0300

    Component Card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Card from "./components/Card";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>INTRODUÇÃO AO REDUX</h1>
+            <div className="row">
+                <Card title="Card1" red>
+                    x
+                </Card>
+                <Card title="Card2" green>
+                    y
+                </Card>
+            </div>
+            <div className="row">
+                <Card title="Card3" blue>
+                    x
+                </Card>
+                <Card title="Card4" purple>
+                    y
+                </Card>
+            </div>
+        </div>
+    );
 }
 
 export default App;
